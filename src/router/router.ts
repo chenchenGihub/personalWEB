8714df8de166389391fe629d964da9570156cbcc
chenchen <chkunshuang@gmail.com>
2018-11-08 22:19:52 +0800

    添加router

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,4 +1,25 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import { IonicVueRouter } from '@modus/ionic-vue'
+import Dashboard from '../pages/dashboard.vue';
+import Edit from '../pages/edit.vue';
 
-Vue.use(Router);
+Vue.use(IonicVueRouter);
+
+
+let routes = {
+    routes:[
+        {
+            name:'main',
+            path:'/main',
+            component:Dashboard,
+        },
+        {
+            name:'edit',
+            path:'/edit',
+            component:Edit,
+        }
+    ]
+}
+
+export default new IonicVueRouter(routes)
