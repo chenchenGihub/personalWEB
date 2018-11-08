08aa3cf3a16299ce5236c7f89462692c79ca295e
chenchen <chkunshuang@gmail.com>
2018-11-08 22:19:12 +0800

    修改main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import Vue from 'vue';
-import Router from 'vue-router';
 import App from './app.vue';
+import { Ionic, IonicVueRouter, IonicAPI } from '@modus/ionic-vue'
+import Routes from './router/router';
+
+import '../static/css/global.scss'
+
+Ionic.init();
+
+Vue.use(IonicAPI)
+
+Vue.config.productionTip = false
+
+Vue.config.ignoredElements.push(/^ion-/)
 
 new Vue({
-    el:'#app',
-    render:h=>h(App)
+    el: '#app',
+    router: Routes,
+    render: h => h(App)
 })
 
 
-  
